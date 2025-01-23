cdc824b2dca98598ce62d7e409a9048abc0c85c7
Jessica Fornetti <93469269+JessicaFornetti@users.noreply.github.com>
2025-01-23 15:36:41 +0000

    Update airQuality.py

--- airQuality.py
+++ airQuality.py
@@ -8,7 +8,7 @@ for v in villes:
     date = datetime.now()
     nomFichier = "/boot/CO2/" + v + date.strftime("%m-%d-%Y_%H:%M:%S")+ ".json"
 
-    with urllib.request.urlopen("https://api.waqi.info/feed/"+ v +"/?token=872697cc1982e50fe12fc319ad65f52f0ea61eda") as url:
+    with urllib.request.urlopen("https://api.waqi.info/feed/"+ v +"/?token=apikey") as url:
         data1 = json.load(url)
 
     with open(nomFichier, "a") as myfile:
